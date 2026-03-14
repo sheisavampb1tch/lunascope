44349cebb952b532214a1983bda85a3fe4e0d2e1
sheisavampb1tch <bloodywhitevuall@gmail.com>
2026-03-14 18:28:05 +0700

    fixd shi

--- app/token/[id]/page.tsx
+++ app/token/[id]/page.tsx
@@ -310,7 +310,7 @@ export default function TokenPage({ params }: { params: Promise<{ id: string }> 
         {/* TOPBAR */}
         <div className="topbar">
           <div className="topbar-left">
-            <a href="/dashboard" className="back-btn">
+            <a href="/" className="back-btn">
               <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5"><path d="m15 18-6-6 6-6"/></svg>
               Back
             </a>
